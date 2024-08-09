a75c910971581b888daa31657d6f20fd2ac036f2
Sergio Rodríguez Calvo <sergiorodriguezcalvo@gmail.com>
2024-08-09 20:12:34 +0200

    Full IT tests

--- src/test/java/dev/serrodcal/customer/infrastructure/CustomerResourceTest.java
+++ src/test/java/dev/serrodcal/customer/infrastructure/CustomerResourceTest.java
@@ -1,5 +1,6 @@
 package dev.serrodcal.customer.infrastructure;
 
+import dev.serrodcal.customers.infrastructure.dtos.AddOrderRequest;
 import dev.serrodcal.customers.infrastructure.dtos.NewCustomerRequest;
 import dev.serrodcal.customers.infrastructure.dtos.UpdateCustomerRequest;
 import io.quarkus.test.junit.QuarkusTest;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 @QuarkusTest
 @TestMethodOrder(OrderAnnotation.class)
@@ -27,7 +29,8 @@ class CustomerResourceTest {
             .when().contentType("application/json")
             .post("/v1/customers")
             .then()
-            .statusCode(201);
+            .statusCode(201)
+            .body("name", equalTo("Sergio"));
     }
 
     @Test
@@ -37,7 +40,8 @@ class CustomerResourceTest {
                 .when().contentType("application/json")
                 .get("/v1/customers")
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+                .body("payload[1].name", equalTo("Sergio"));
     }
 
     @Test
@@ -47,7 +51,8 @@ class CustomerResourceTest {
                 .when().contentType("application/json")
                 .get("/v1/customers/1")
                 .then()
-                .statusCode(200);
+                .statusCode(200)
+                .body("name", equalTo("Sergio"));
     }
 
     @Test
@@ -62,6 +67,29 @@ class CustomerResourceTest {
                 .body(body)
                 .when().contentType("application/json")
                 .put("/v1/customers/1")
+                .then()
+                .statusCode(204);
+    }
+
+    @Test
+    @Order(5)
+    void testAddOrderEndpoint() {
+        AddOrderRequest body = new AddOrderRequest("phone", 1);
+
+        given()
+                .body(body)
+                .when().contentType("application/json")
+                .post("/v1/customers/1/orders")
+                .then()
+                .statusCode(201);
+    }
+
+    @Test
+    @Order(6)
+    void testRemoveOrderEndpoint() {
+        given()
+                .when().contentType("application/json")
+                .delete("/v1/customers/1/orders/1")
                 .then()
                 .statusCode(204);
     }
